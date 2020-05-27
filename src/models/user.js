a093b9fc52ce7f4f0dc9ff4f51f6428846b5ccad
Mitchell Goodwin <mgoodwin777@gmail.com>
2020-05-27 16:31:01 -0500

    Create User Route

--- src/models/user.js
+++ src/models/user.js
@@ -19,7 +19,13 @@ const userSchema = new mongoose.Schema({
                 throw new Error('Password is invalid')
             }
         }
-    }
+    },
+    tokens: [{
+        token: {
+            type: String,
+            required: true
+        }
+    }]
 })
 
 userSchema.pre('save', async function (next) {
@@ -31,6 +37,16 @@ userSchema.pre('save', async function (next) {
 
     next()
 })
+
+userSchema.methods.generateAuthToken = async function () {
+    const user = this
+    const token = jwt.sign({ _id: user._id.toString() }, process.env.SECRET)
+
+    user.tokens = user.tokens.concat({ token })
+    await user.save()
+
+    return token
+}
 
 const User = mongoose.model('User', userSchema)
 
---
+++ src/routers/user.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const router = new express.Router()
+const User = require('../models/user')
+
+router.post('/users', async (req, res) => {
+    console.log(req.body)
+    const user = new User(req.body)
+
+    try {
+        await user.save()
+        const token = await user.generateAuthToken()
+        res.status(201).send({ user, token })
+    } catch (e) {
+        res.status(400).send(e)
+    }
+})
+
+module.exports = router

b6d38529beeb1bcd147757748f8042a425f607e9
Mitchell Goodwin <mgoodwin777@gmail.com>
2020-05-27 16:34:43 -0500

    Auth routes

--- src/routers/user.js
+++ src/routers/user.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const router = new express.Router()
 const User = require('../models/user')
+const auth = require('../middleware/auth')
 
 router.post('/users', async (req, res) => {
     console.log(req.body)
@@ -12,6 +13,27 @@ router.post('/users', async (req, res) => {
         res.status(201).send({ user, token })
     } catch (e) {
         res.status(400).send(e)
+    }
+})
+
+router.post('/users/login', async (req, res) => {
+    try {
+        const user = await User.findByCredentials(req.body.email, req.body.password)
+        const token = await user.generateAuthToken()
+        res.send({ user, token })
+    } catch (e) {
+        res.status(400).send()
+    }
+})
+
+router.post('/users/logout', auth, async (req, res) => {
+    try {
+        req.user.tokens = req.user.tokens.filter(token => token.token !== req.token)
+        await req.user.save()
+
+        res.send()
+    } catch (e) {
+        res.status(500).send()
     }
 })
 

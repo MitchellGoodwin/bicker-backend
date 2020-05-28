4cdc5196531ab2066bd85a2cc0607f3cdb30bd82
Mitchell Goodwin <mgoodwin777@gmail.com>
2020-05-27 19:52:54 -0500

    Added getting all posts

--- src/routers/post.js
+++ src/routers/post.js
@@ -17,4 +17,40 @@ router.post('/posts', auth, async (req, res) => {
     }
 })
 
+router.patch('/posts/:id', auth, async (req, res) => {
+    const updates = Object.keys(req.body)
+    const allowedUpdates = ['text']
+    const isValidOperation = updates.every((update) => {
+        return allowedUpdates.includes(update)
+    })
+
+    if (!isValidOperation) {
+        return res.status(400).send({ error: 'Invalid updates!'})
+    }
+
+    const _id = req.params.id
+
+    try {
+        const post = await Post.findOne({ _id, owner: req.user._id})
+        
+        if (!post) {
+            return res.status(400).send()
+        }
+
+        updates.forEach(update => {
+            post[update] = req.body[update]
+        })
+        await post.save()
+
+        res.send(post)
+    } catch (e) {
+        res.status(400).send()
+    }
+})
+
+router.get('/posts', async (req, res) => {
+    const posts = await Post.find().sort({createdAt: -1}).populate('owner')
+    res.send(posts)
+})
+
 module.exports = router
